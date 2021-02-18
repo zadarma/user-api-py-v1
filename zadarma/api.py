fd55d955d2cd4e1bb01d46ab453a62a9c8c86aa4
Andrey-Shatunov <andreshatuno@gmail.com>
2021-02-18 12:15:13 +0500

    Update api.py

--- zadarma/api.py
+++ zadarma/api.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 import sys
 from hashlib import sha1, md5
 from collections import OrderedDict
@@ -28,7 +28,46 @@ class ZadarmaAPI(object):
         if is_sandbox:
             self.__url_api = 'https://api-sandbox.zadarma.com'
 
-    def http_build_query(self, data):
+    def call(self, method, params={}, request_type='GET', format='json', is_auth=True):
+        """
+        Function for send API request
+        :param method: API method, including version number
+        :param params: Query params
+        :param request_type: (get|post|put|delete)
+        :param format: (json|xml)
+        :param is_auth: (True|False)
+        :return: response
+        """
+        request_type = request_type.upper()
+        if request_type not in ['GET', 'POST', 'PUT', 'DELETE']:
+            request_type = 'GET'
+        params['format'] = format
+        auth_str = None
+        is_nested_data = False
+        for k in params.values():
+            if not isinstance(k, str):
+                is_nested_data = True
+                break
+        if is_nested_data:
+            params_string = self.__http_build_query(OrderedDict(sorted(params.items())))
+            params = params_string
+        else:
+            params_string = urlencode(OrderedDict(sorted(params.items())))
+
+        if is_auth:
+            auth_str = self.__get_auth_string_for_header(method, params_string)
+
+        if request_type == 'GET':
+            result = requests.get(self.__url_api + method + '?' + params_string, headers={'Authorization': auth_str})
+        elif request_type == 'POST':
+            result = requests.post(self.__url_api + method, headers={'Authorization': auth_str}, data=params)
+        elif request_type == 'PUT':
+            result = requests.put(self.__url_api + method, headers={'Authorization': auth_str}, data=params)
+        elif request_type == 'DELETE':
+            result = requests.delete(self.__url_api + method, headers={'Authorization': auth_str}, data=params)
+        return result.text
+
+    def __http_build_query(self, data):
         parents = list()
         pairs = dict()
 
@@ -57,52 +96,13 @@ class ZadarmaAPI(object):
             return pairs
         return urlencode(r_urlencode(data))
 
-    def call(self, method, params={}, request_type='GET', format='json', is_auth=True):
-        """
-        Function for send API request
-        :param method: API method, including version number
-        :param params: Query params
-        :param request_type: (get|post|put|delete)
-        :param format: (json|xml)
-        :param is_auth: (True|False)
-        :return: response
-        """
-        request_type = request_type.upper()
-        allowed_type = ['GET', 'POST', 'PUT', 'DELETE']
-        if request_type not in allowed_type:
-            request_type = 'GET'
-        params['format'] = format
-        auth_str = None
-        if is_auth:
-            auth_str = self.__get_auth_string_for_header(method, params)
-
-        result = False
-
-        sorted_dict_params = OrderedDict(sorted(params.items()))
-        params_string = self.http_build_query(sorted_dict_params)
-
-        if request_type == 'GET':
-            request_url = self.__url_api + method + '?' + params_string
-            result = requests.get(request_url, headers={'Authorization': auth_str})
-        elif request_type == 'POST':
-            result = requests.post(self.__url_api + method, headers={'Authorization': auth_str}, data=params_string)
-        elif request_type == 'PUT':
-            result = requests.put(self.__url_api + method, headers={'Authorization': auth_str}, data=params_string)
-        elif request_type == 'DELETE':
-            result = requests.delete(self.__url_api + method, headers={'Authorization': auth_str}, data=params_string)
-        print("result: " + str(result.text))
-        return result.text
-
-    def __get_auth_string_for_header(self, method, params):
+    def __get_auth_string_for_header(self, method, params_string):
         """
         :param method: API method, including version number
         :param params: Query params dict
         :return: auth header
         """
-        sorted_dict_params = OrderedDict(sorted(params.items()))
-        params_string = self.http_build_query(sorted_dict_params)
-        md5hash = md5(params_string.encode('utf8')).hexdigest()
-        data = method + params_string + md5hash
+        data = method + params_string + md5(params_string.encode('utf8')).hexdigest()
         hmac_h = hmac.new(self.secret.encode('utf8'), data.encode('utf8'), sha1)
         if sys.version_info.major > 2:
             bts = bytes(hmac_h.hexdigest(), 'utf8')
